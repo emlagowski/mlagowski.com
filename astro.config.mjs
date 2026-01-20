80470f7bc66209a8aabe866d58a372a7a9ea95b9
Marcin Łagowski <marcinlagowski92@gmail.com>
2026-01-20 02:17:38 +0100

    changed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mlagowski.com',
+  site: 'https://emlagowski.github.io',
+  base: '/mlagowski.com',
   output: 'static',
   build: {
     format: 'directory'
