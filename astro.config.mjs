83e474e062b75a28c17ed5dc45c67ea2c5715227
Marcin Łagowski <marcinlagowski92@gmail.com>
2026-03-13 23:44:35 +0100

    added sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emlagowski.github.io',
   base: '/',
   output: 'static',
+  integrations: [sitemap()],
   build: {
     format: 'directory'
   }
